bbfa96105687180dfc91213b5cff63c492f8bc90
quantrananh2304 <65522530+quantrananh2304@users.noreply.github.com>
2021-11-03 01:17:28 +0700

    user collection

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,23 +1,24 @@
-const { checkToken } = require('../../validation/checkToken');
+const { checkToken } = require("../../validation/checkToken");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys');
-const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load input validation
-const validateRegisterInput = require('../../validation/register');
-const validateLoginInput = require('../../validation/login');
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+const validateAddInput = require("../../validation/userValidation");
 
 // Load User model
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
     // Form validation
 
     const { errors, isValid } = validateRegisterInput(req.body);
@@ -29,7 +30,7 @@ router.post('/register', (req, res) => {
 
     User.findOne({ email: req.body.email }).then((user) => {
         if (user) {
-            return res.status(400).json({ email: 'Email already exists' });
+            return res.status(400).json({ email: "Email already exists" });
         } else {
             const newUser = new User({
                 name: req.body.name,
@@ -55,7 +56,7 @@ router.post('/register', (req, res) => {
 // @route POST api/users/login
 // @desc Login user and return JWT token
 // @access Public
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
     // Form validation
 
     const { errors, isValid } = validateLoginInput(req.body);
@@ -72,7 +73,7 @@ router.post('/login', (req, res) => {
     User.findOne({ email }).then((user) => {
         // Check if user exists
         if (!user) {
-            return res.status(404).json({ emailNotFound: 'Email not found' });
+            return res.status(404).json({ emailNotFound: "Email not found" });
         }
 
         // Check password
@@ -85,8 +86,6 @@ router.post('/login', (req, res) => {
                     name: user.name,
                 };
 
-                console.log('payload', payload);
-
                 // Sign token
                 jwt.sign(
                     payload,
@@ -97,29 +96,134 @@ router.post('/login', (req, res) => {
                     (err, token) => {
                         res.json({
                             success: true,
-                            token: 'Bearer ' + token,
+                            token: token,
                         });
                     }
                 );
             } else {
                 return res
                     .status(400)
-                    .json({ passwordIncorrect: 'Password incorrect' });
+                    .json({ passwordIncorrect: "Password incorrect" });
             }
         });
     });
 });
 
 const updateUser = (req, res, next) => {
-    console.log('req', req);
     if (req.user) {
         return next();
     }
-    res.status(401).send('Unauthorized!');
+
+    res.status(401).json({ msg: "Unauthorized!" });
 };
 
-router.post('/updateUser', checkToken, updateUser, (req, res) => {
-    res.status(200).send(req.user);
+router.post("/updateUser", checkToken, updateUser, (req, res) => {
+    const { errors, isValid } = validateAddInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const { _id, email, name } = req.body;
+
+    const filter = _id;
+    const update = { email, name };
+
+    User.findByIdAndUpdate(
+        filter,
+        update,
+        { new: true, useFindAndModify: false },
+        (err, doc) => {
+            // res.status(200).json({ msg: "Successfully added!" });
+            if (err) {
+                res.json({ msg: err });
+            } else {
+                res.status(200).json({
+                    msg: "Successfully updated!",
+                    user: doc,
+                });
+            }
+        }
+    );
+});
+
+const getListUser = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+
+    res.status(401).json({ msg: "Unauthorized!" });
+};
+
+router.post("/getListUser", checkToken, getListUser, (req, res) => {
+    User.find(req.body).then((user) => {
+        if (user.length !== 0) {
+            return res.status(200).json(user);
+        } else {
+            return res.status(200).json({ msg: "No user found!" });
+        }
+    });
+});
+
+const insertUser = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+
+    res.status(401).json({ msg: "Unauthorized!" });
+};
+
+router.post("/insertUser", checkToken, insertUser, (req, res) => {
+    const { errors, isValid } = validateAddInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const { email, name } = req.body;
+
+    User.findOne({ email }).then((user) => {
+        if (user) {
+            return res.status(200).json({ email: "Email already exists!" });
+        } else {
+            const newUser = new User({
+                name,
+                email,
+                password: "123456",
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(() => res.json({ msg: "Successfully added!" }))
+                        .catch((err) => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+const deleteUser = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+
+    res.status(401).json({ msg: "Unauthorized!" });
+};
+
+router.post("/deleteUser", checkToken, deleteUser, (req, res) => {
+    const filter = req.body._id;
+
+    User.findByIdAndDelete(filter, (err, doc) => {
+        if (err) {
+            res.json({ msg: err });
+        } else {
+            res.status(200).json({ msg: "Successfully deleted!" });
+        }
+    });
 });
 
 module.exports = router;
--- validation/checkToken.js
+++ validation/checkToken.js
@@ -1,25 +1,24 @@
-const jwt = require('jsonwebtoken');
-const { secretOrKey } = require('../config/keys');
+const jwt = require("jsonwebtoken");
+const { secretOrKey } = require("../config/keys");
 
 const checkToken = (req, res, next) => {
     try {
-        const token = req.headers.authorization;
+        const token = req.headers.authorization.split(" ")[1];
 
         // Xác thực token
         jwt.verify(token, secretOrKey, (err, payload) => {
-            console.log('payload', payload);
             if (payload) {
                 req.user = payload;
                 next();
             } else {
                 // Nếu token tồn tại nhưng không hợp lệ, server sẽ response status code 401 với msg bên dưới
-                res.status(401).send('Unauthorized');
+                res.status(401).send("Unauthorized");
             }
         });
     } catch (err) {
         // Nếu không có token ở header, server sẽ response status code 401 với msg bên dưới
-        console.log('err', err);
-        res.status(401).send('No token provided');
+        console.log("err", err);
+        res.status(401).send("No token provided");
     }
 };
 
